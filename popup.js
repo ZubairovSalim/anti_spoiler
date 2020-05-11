6ed9af96f20270a1139b2bbf016a135d0eb06172
salim <zubairov.salim@yandex.ru>
2020-05-11 23:11:14 +0500

    added interface for adding and removing keywords

---
+++ popup.js
@@ -0,0 +1,49 @@
+let storage = chrome.storage.sync;
+let keywords = [];
+
+$(function(){
+    function updateKeywordsListNode() {
+        storage.get("keywords", function(items){
+            if (items.keywords) {
+                keywords = items.keywords.slice();
+                let nodeList = items.keywords.map((keyword, index) =>
+                    "<li id=" + "key" + index + " style='list-style: none; display: flex; justify-content: space-between '>" +
+                    keyword + "<button id="+ "b" + index + " class='del-keyword'>-</button</li>");
+                $("#keyword-list").html(nodeList);
+            }
+        });
+    }
+
+    $("#add-keyword").click(function(){
+        let new_keyword = $("#new-keyword").val();
+
+        if(!new_keyword || keywords.includes(new_keyword)) {
+            return false;
+        }
+
+        keywords.push(new_keyword);
+        $("#new-keyword").val("");
+
+        storage.set({
+            keywords: keywords
+        });
+
+        updateKeywordsListNode();
+    });
+
+    $("#keyword-list").on("click", ".del-keyword", function () {
+        let delKeywordId = $(this).attr("id").slice(1);
+        let liNode = $("#key" + delKeywordId);
+        let delKeyword = liNode[0].firstChild.data;
+
+        keywords.splice(keywords.indexOf(delKeyword), 1);
+
+        storage.set({
+            keywords: keywords
+        });
+
+        updateKeywordsListNode();
+    });
+
+    updateKeywordsListNode();
+});
