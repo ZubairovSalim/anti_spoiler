3ef568a1195a1fc3e9d3d1dcd66fe8219d119a9a
aleksej <al1111997@yandex.ru>
2020-05-12 23:33:41 +0500

    added spoiler alert field and separation in switch-case

--- content.js
+++ content.js
@@ -1,15 +1,49 @@
+let html = `<div class='spoiler'>SPOILER ALERT <a href='javascript: void(0)' alt='' >show</a></div>`
+
 function parserGo() {
-    analysisSite(document);
+    analysisSite(document)
+}
+
+function blurContent(data, block) {
+    $(data).find(block).each(function () {
+        $(this).find('span.st').addClass('blurClass')
+        $(this).find('label.extended-text').addClass('blurClass')
+        $(this).find('div.organic__text').addClass('blurClass')
+        $(this).find('sitelinks').css("display", "none")
+        $(this).append(html)
+    })
+}
+
+function checkLink(link) {
+    if (link.includes('google')) {
+        return 'google'
+    } else if (link.includes('vk')) {
+        return 'vk'
+    } else {
+        return 'yandex'
+    }
 }
 
 function analysisSite(data) {
-    //test blur for google earch results
-    $(data).find('div.s').each(function(){
-        $(this).css("filter", "blur(10px)");
-    });
+    let link = data.location.hostname
+    link = link.toString()
+    let checked = checkLink(link)
+
+    switch (checked) {
+        case 'google':
+            blurContent(data, 'div.s')
+            break
+        case 'yandex':
+            blurContent(data, 'div.organic__content-wrapper')
+            break
+        case 'vk':
+            console.log('www.vk.com')
+            break
+        default: console.log('def'); break
+    }
 }
 
-parserGo();
+parserGo()
 
 
 
