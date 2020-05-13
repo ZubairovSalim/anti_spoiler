08dd52bdfdc1d2d2bb11c87a2c90ca81ec016208
aleksej <al1111997@yandex.ru>
2020-05-13 20:18:05 +0500

    added logic for settings screen and preparin to work with keywords

--- content.js
+++ content.js
@@ -1,6 +1,28 @@
+let st = chrome.storage.sync
+let keys = []
+let settings = {}
+
+st.get(item => {
+    keys = item.keywords
+    settings = item.settings
+})
+
+st.set({
+    ...st,
+    ...settings
+})
 
 $(function () {
-    let elems = []
+    for (let i in keys) {
+        keys[i] = keys[i].toLowerCase()
+    }
+
+    console.log(keys);
+    console.log(settings['s1'])
+    console.log(settings['s2'])
+    console.log(settings['s3'])
+
+    let keysCount = keys.length
     let counter = 0
     function parserGo() {
         analysisSite(document)
@@ -8,18 +30,22 @@ $(function () {
 
     function blurContent(data, block) {
         $(data).find(block).each(function () {
+            let link = data.location.hostname
             let html = `<div class='spoiler' id='spoiler${counter}'>SPOILER ALERT <a href='#' id='showLink_${counter}' class='showLink' alt='' >show</a></div>`
+            //google
             $(this).find('span.st').addClass(['blurClass'])
             $(this).find('span.st').attr('id', `showText${counter}`)
+            //yandex
             $(this).find('label.extended-text').addClass(['blurClass'])
             $(this).find('label.extended-text').attr('id', `showText${counter}`)
             $(this).find('div.organic__text').addClass(['blurClass'])
             $(this).find('div.organic__text').attr('id', `showText${counter}`)
-            $(this).find('sitelinks').css("display", "none")
-            let el = document.getElementById(`#showText${counter}`)
-            elems.push(el)
+
             $(this).append(html)
             counter += 1
+            //vk - coming soon
+            //------
+
         })
     }
 
@@ -34,16 +60,17 @@ $(function () {
     }
 
     function analysisSite(data) {
+
         let link = data.location.hostname
         link = link.toString()
         let checked = checkLink(link)
 
         switch (checked) {
             case 'google':
-                blurContent(data, 'div.s')
+                settings['s1'] ? blurContent(data, 'div.s') : null
                 break
             case 'yandex':
-                blurContent(data, 'div.organic__content-wrapper')
+                settings['s2'] ? blurContent(data, 'div.organic__content-wrapper') : null
                 break
             case 'vk':
                 console.log('www.vk.com')
@@ -54,7 +81,27 @@ $(function () {
 
     parserGo()
 
-    $(`a.showLink`).on("click", function (e) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $(`a.showLink`).on("click", e => {
         let id = e.target.id.split('_')[1]
         e.preventDefault()
         $(document).find(`span#showText${id}`).removeClass('blurClass')
@@ -64,6 +111,51 @@ $(function () {
         return false
     });
 
+
+    $(`a#changePage`).on("click", e => {
+        e.preventDefault()
+        let x = $("#settings")[0]
+        let y = $("#main")[0]
+        let z = $('#changePage')[0]
+        if (x.style.display === "none") {
+            x.style.display = "flex"
+            y.style.display = "none"
+            z.innerHTML = 'back'
+        } else {
+            x.style.display = "none"
+            y.style.display = "block"
+            z.innerHTML = 'settings'
+        }
+        return false
+    });
+
+    $('#setting1, #setting2, #setting3').change(function (e) {
+        if ($(this).is(':checked')) {
+            settings[$(this).attr('name')] = true
+            st.set({
+                ...st,
+                settings: settings
+            })
+        } else {
+            settings[$(this).attr('name')] = false
+            st.set({
+                ...st,
+                settings: settings
+            })
+        }
+    });
+
+
+    // ОТСЛЕЖИВАНИЕ СТЕЙТА (ПРИ ОБНОВЛЕНИИ СТРАНИЦЫ)
+    // st.get('keywords', k => {
+    //     console.log(k)
+    // })
+    st.get('settings', s => {
+        for (let i in s) {
+            $('#setting1').prop('checked', s[i].s1)
+            $('#setting2').prop('checked', s[i].s2)
+            $('#setting3').prop('checked', s[i].s3)
+        }
+        // console.log(s)
+    })
 })
-
-
